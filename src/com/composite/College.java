708a0b96bae8bef3f6be47f7affdfa6eeef1cd19
Trishul <trishhgowda@gmail.com>
2024-05-14 10:36:14 +0530

    Composite design pattern

---
+++ src/com/composite/College.java
@@ -0,0 +1,6 @@
+package com.composite;
+
+public interface College {
+
+	public int numberOfStudents();
+}
---
+++ src/com/composite/IndependentCollege.java
@@ -0,0 +1,19 @@
+package com.composite;
+
+public class IndependentCollege implements College {
+
+	String name;
+	int numberOfStudents;
+	
+	public IndependentCollege(String name, int numberOfStudents) {
+		super();
+		this.name = name;
+		this.numberOfStudents = numberOfStudents;
+	}
+
+	@Override
+	public int numberOfStudents() {
+		return this.numberOfStudents;
+	}
+
+}
